b6be5dc6b6da58a34271e5b9892c941de8232abc
Jin Joo Kim <jinjookim@Jinui-noteubug.local>
2025-03-25 17:12:02 +0900

    🎉 chore: 프로젝트 생성 및 기본 환경 설정 (spring boot, jdk17, gradle)

---
+++ src/test/java/com/jinjoo/salarysim/salarysim/SalarysimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinjoo.salarysim.salarysim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalarysimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
